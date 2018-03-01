a4de9346c18b5840156c8aaf57ac19fd003a62b7
gatoko <g.millaquiensuarez@gmail.com>
2018-03-01 00:25:52 -0300

    ejercicio finalizado

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Escribir un método que acepte dos strings y devuelva el string con más caracteres. Si ambos tienen la misma cantidad de caracteres, devolver cualquiera de los dos.
+
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                try
+                {
+                    Console.WriteLine("Escriba una palabra : ");
+                    string palabra1 = Console.ReadLine();
+                    Console.WriteLine("Escriba otra  palabra : ");
+                    string palabra2 = Console.ReadLine();
+
+                    if (palabra1.Length>palabra2.Length)
+                    {
+                        Console.WriteLine("La palabra "+palabra1 + " tiene más caracteres que la palabra " + palabra2);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("La palabra " + palabra2 + " tiene más caracteres que la palabra " + palabra1);
+
+                    }
+
+                    break;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Escibre algo bien XD ");
+                }
+            }
+
+
+            Console.Read();
+        }
+    }
+
+
+    }
+
